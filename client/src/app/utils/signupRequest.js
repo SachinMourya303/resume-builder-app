37b2857aa211575f403a775d6115dad03ff1ba8f
Sachin <SachinMourya303@gmail.com>
2025-11-20 13:31:19 +0530

    Updated the main dotenv

--- client/src/app/utils/signupRequest.js
+++ client/src/app/utils/signupRequest.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify'
 
 export const signupRequest = async (dispatch, name, email, password) => {
     try {
-        const response = await axios.post(`${import.meta.env.SERVER_API}/auth/signup`, { dispatch, name, email, password });
+        const response = await axios.post(`${import.meta.env.VITE_API_URI}/auth/signup`, { dispatch, name, email, password });
         dispatch(logIn(response.data));
         toast.success(response.data.message);
     } catch (error) {
